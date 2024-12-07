3b2a8cd9a1063f31753ff626d5ca2a64afdf2c85
Dhruv Patel <dhruvp5200@gmail.com>
2024-12-08 01:01:10 +0530

    Removed unnecessary properties from Movie data class

--- app/src/main/java/com/android/assignment/models/Movie.kt
+++ app/src/main/java/com/android/assignment/models/Movie.kt
@@ -7,30 +7,10 @@ data class Movie(
     val id: Int,
     @SerializedName("title")
     val title: String,
-    @SerializedName("original_title")
-    val originalTitle: String,
     @SerializedName("overview")
     val overview: String,
     @SerializedName("poster_path")
     val posterPath: String,
     @SerializedName("backdrop_path")
     val backdropPath: String?,
-    @SerializedName("release_date")
-    val releaseDate: String?,
-    @SerializedName("genre_ids")
-    val genreIds: List<Int>,
-    @SerializedName("original_language")
-    val originalLanguage: String,
-    @SerializedName("media_type")
-    val mediaType: String,
-    @SerializedName("adult")
-    val adult: Boolean,
-    @SerializedName("popularity")
-    val popularity: Double,
-    @SerializedName("vote_average")
-    val voteAverage: Double,
-    @SerializedName("vote_count")
-    val voteCount: Int,
-    @SerializedName("video")
-    val video: Boolean
 )
